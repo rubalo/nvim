e899ac525e0aa7a84c086fafc494f171bb083c1e
rubalo <rubalo@users.noreply.github.com>
2023-12-21 15:41:00 +0100

    Change color scheme

--- lua/rubalo/init.lua
+++ lua/rubalo/init.lua
@@ -1,2 +1,5 @@
 require("rubalo.remap")
 require("rubalo.set")
+
+vim.o.background = "dark" -- or "light" for light mode
+vim.cmd([[colorscheme gruvbox]])
--- lua/rubalo/packer.lua
+++ lua/rubalo/packer.lua
@@ -13,17 +13,13 @@ return require('packer').startup(function(use)
 	  requires = { {'nvim-lua/plenary.nvim'} }
   }
 
-  use({
-	  'folke/tokyonight.nvim', 
-	  as  = 'tokyonight',
-	  config = function()
-		  vim.cmd('colorscheme tokyonight')
-	  end
-  })
-
   use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+  
   use('mbbill/undotree')
+  
   use('tpope/vim-fugitive')
+
+  use { "ellisonleao/gruvbox.nvim" }
 
   use {
 	  'VonHeikemen/lsp-zero.nvim',
