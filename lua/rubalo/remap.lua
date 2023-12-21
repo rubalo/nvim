419e7de434267931038b4547a7e86c887d2b9e9e
rubalo <rubalo@users.noreply.github.com>
2023-12-21 13:31:33 +0100

    Update gitignore and add clipboard yank shortcut

--- lua/rubalo/remap.lua
+++ lua/rubalo/remap.lua
@@ -1,5 +1,15 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
+-- Move Text in up and down in Visual mode
+--
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")     
 vim.keymap.set("v", "K", ":m '<-2,<CR>gv=gv")     
+
+-- greatest remap ever
+vim.keymap.set("x", "<leader>p", [["_dP]])
+
+-- next greatest remap ever : asbjornHaland
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+
